c3a840241323118c7face3567b32943bd0337371
Mahmoud Ali <eng.mahmoudali204@gmail.com>
2023-06-05 04:08:47 +0300

    add 404 custom page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.main')
+
+@section('title', '404 Page Not Found')
+
+
+@section('content')
+
+<h1 style="font-size: 10.5em;">404</h1>
+
+<h3 style="font-size: 3em;">Page Not Found</h3>
+
+@endsection
